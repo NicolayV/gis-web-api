d789757036615d0f1899fa7f6f1df6bff00a0d81
Velgosh Nicolay <velgoshnicolay@MacBook-Pro-Velgosh.local>
2022-01-19 22:57:17 +0300

    mod-router

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,13 +1,15 @@
 import React, { useContext } from "react";
-import { Navigate, useRoutes } from "react-router-dom";
+import { Navigate, Outlet, useRoutes } from "react-router-dom";
 import { AuthContext } from "../context";
 import LoginPage from "../pages/LoginPage/";
 import GisMap from "../pages/MapPage/";
 
-const AppRouter = () => {
+const AuthProvider = ({ route }) => {
   const { isAuth } = useContext(AuthContext);
-  console.log("isAuth", isAuth);
+  return isAuth ? <Outlet /> : route;
+};
 
+const AppRouter = () => {
   const routesPublic = useRoutes([
     { path: "/login", element: <LoginPage /> },
     { path: "/*", element: <Navigate to="/login" /> },
@@ -18,11 +20,7 @@ const AppRouter = () => {
     { path: "/*", element: <Navigate to="/gismap" /> },
   ]);
 
-  const GuardRoute = ({ children }) => {
-    return isAuth ? children : routesPublic;
-  };
-
-  return <GuardRoute> {routesPrivate}</GuardRoute>;
+  return <AuthProvider route={routesPublic}>{routesPrivate}</AuthProvider>;
 };
 
 export default AppRouter;

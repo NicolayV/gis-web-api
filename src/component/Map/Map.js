f0704792e52acfbf0b5c43b82130623f1816dbea
Velgosh Nicolay <velgoshnicolay@MacBook-Pro-Velgosh.local>
2022-01-23 22:36:51 +0300

    map-init

---
+++ src/component/Map/Map.js
@@ -0,0 +1,36 @@
+import React, { useCallback, useRef } from "react";
+import { GoogleMap } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "100%",
+  height: "100%",
+};
+
+const Map = ({ center }) => {
+  const mapRef = useRef(undefined);
+
+  const onLoad = useCallback(function callback(map) {
+    mapRef.current = map;
+  }, []);
+
+  const onUnmount = useCallback(function callback(map) {
+    mapRef.current = undefined;
+  }, []);
+
+  return (
+    <div>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        {/* Child components, such as markers, info windows, etc. */}
+        <></>
+      </GoogleMap>
+    </div>
+  );
+};
+
+export { Map };
